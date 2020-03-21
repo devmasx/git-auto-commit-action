9fda2dd13395740dd54e504deecac54fca2d1328
miguemasx <migue.masx@gmail.com>
2020-03-21 20:05:39 +0100

    dev

---
+++ entrypoint.cr
@@ -0,0 +1,31 @@
+def git_is_dirty
+  result = `git status -s`
+  puts result
+end
+
+git_is_dirty
+# # switch_to_repository
+# echo "INPUT_REPOSITORY value: $INPUT_REPOSITORY";
+# cd $INPUT_REPOSITORY
+
+# if _git_is_dirty; then
+#     BRANCH=$(echo "$GITHUB_REF" | sed "s/refs\/heads\///")
+
+#     # Set up .netrc file with GitHub credentials
+#     cat <<- EOF > $HOME/.netrc
+#         machine github.com
+#         login $GITHUB_ACTOR
+#         password $GITHUB_TOKEN
+# EOF
+#     chmod 600 $HOME/.netrc
+#     git config --global user.email "actions@github.com"
+#     git config --global user.name "GitHub Actions"
+
+#     git checkout $BRANCH
+#     git add "${INPUT_FILE_PATTERN}"
+#     git commit -m "$INPUT_COMMIT_MESSAGE" \
+#     --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
+#     git push --set-upstream origin "HEAD:$BRANCH"
+# else
+#     echo "Working tree clean. Nothing to commit."
+# fi
